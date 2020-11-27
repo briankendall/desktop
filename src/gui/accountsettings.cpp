owncloud/client

BigFolders: Enable "apply manual changes" only if dirty
Currently it's confusing because without user changes "Sync None" and
"Apply manual changes" have the same meaning. Now the "Apply" action
only becomes available if changes were made.

For #7230

src/gui/accountsettings.cpp
@@ -985,6 +985,7 @@ void AccountSettings::refreshSelectiveSyncStatus()
 
         ui->selectiveSyncButtons->setVisible(true);
         ui->bigFolderUi->setVisible(false);
+        ui->selectiveSyncApply->setEnabled(_model->isDirty());
     } else {
         // There's a reason the big folder ui should be shown
         shouldBeVisible = _accountState->isConnected();
@@ -999,9 +1000,9 @@ void AccountSettings::refreshSelectiveSyncStatus()
         ui->selectiveSyncNotification->setText(info + msg);
         ui->selectiveSyncButtons->setVisible(false);
         ui->bigFolderUi->setVisible(true);
+        ui->bigFolderApply->setEnabled(_model->isDirty());
     }
 
-    ui->selectiveSyncApply->setEnabled(_model->isDirty() || !msg.isEmpty());
     bool wasVisible = !ui->selectiveSyncStatus->isHidden();
     if (wasVisible != shouldBeVisible) {
         QSize hint = ui->selectiveSyncStatus->sizeHint();

briankendall/desktop

src/gui/accountsettings.cpp
@@ -1108,6 +1108,7 @@ void AccountSettings::refreshSelectiveSyncStatus()
 
         _ui->selectiveSyncButtons->setVisible(true);
         _ui->bigFolderUi->setVisible(false);
+        _ui->selectiveSyncApply->setEnabled(_model->isDirty());
     } else {
         // There's a reason the big folder ui should be shown
         shouldBeVisible = _accountState->isConnected();
@@ -1122,9 +1123,9 @@ void AccountSettings::refreshSelectiveSyncStatus()
         _ui->selectiveSyncNotification->setText(info + msg);
         _ui->selectiveSyncButtons->setVisible(false);
         _ui->bigFolderUi->setVisible(true);
+        _ui->bigFolderApply->setEnabled(_model->isDirty());
     }
 
-    _ui->selectiveSyncApply->setEnabled(_model->isDirty() || !msg.isEmpty());
     bool wasVisible = !_ui->selectiveSyncStatus->isHidden();
     if (wasVisible != shouldBeVisible) {
         QSize hint = _ui->selectiveSyncStatus->sizeHint();