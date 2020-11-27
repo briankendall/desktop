ckamm/mirall

Pin state updates
- unspecified and inherited are different
- move enum to header in common/
- access through Vfs instead of directly in Journal

src/gui/socketapi.cpp
@@ -615,9 +615,8 @@ void SocketApi::command_MAKE_AVAILABLE_LOCALLY(const QString &filesArg, SocketLi
             continue;
 
         // Update the pin state on all items
-        auto pinPath = data.folderRelativePathNoVfsSuffix().toUtf8();
-        data.folder->journalDb()->wipePinStateForPathAndBelow(pinPath);
-        data.folder->journalDb()->setPinStateForPath(pinPath, PinState::AlwaysLocal);
+        auto pinPath = data.folderRelativePathNoVfsSuffix();
+        data.folder->vfs().setPinState(pinPath, PinState::AlwaysLocal);
 
         // Trigger the recursive download
         data.folder->downloadVirtualFile(data.folderRelativePath);
@@ -635,9 +634,8 @@ void SocketApi::command_MAKE_ONLINE_ONLY(const QString &filesArg, SocketListener
             continue;
 
         // Update the pin state on all items
-        auto pinPath = data.folderRelativePathNoVfsSuffix().toUtf8();
-        data.folder->journalDb()->wipePinStateForPathAndBelow(pinPath);
-        data.folder->journalDb()->setPinStateForPath(pinPath, PinState::OnlineOnly);
+        auto pinPath = data.folderRelativePathNoVfsSuffix();
+        data.folder->vfs().setPinState(pinPath, PinState::OnlineOnly);
 
         // Trigger recursive dehydration
         data.folder->dehydrateFile(data.folderRelativePath);
@@ -913,7 +911,7 @@ void SocketApi::command_GET_MENU_ITEMS(const QString &argument, OCC::SocketListe
         for (const auto &file : files) {
             auto fileData = FileData::get(file);
             auto path = fileData.folderRelativePathNoVfsSuffix();
-            auto pinState = folder->journalDb()->effectivePinStateForPath(path.toUtf8());
+            auto pinState = folder->vfs().getPinState(path);
             if (!pinState) {
                 // db error
                 hasAlwaysLocal = true;

briankendall/desktop

src/gui/socketapi.cpp
@@ -700,9 +700,8 @@ void SocketApi::command_MAKE_AVAILABLE_LOCALLY(const QString &filesArg, SocketLi
             continue;
 
         // Update the pin state on all items
-        auto pinPath = data.folderRelativePathNoVfsSuffix().toUtf8();
-        data.folder->journalDb()->wipePinStateForPathAndBelow(pinPath);
-        data.folder->journalDb()->setPinStateForPath(pinPath, PinState::AlwaysLocal);
+        auto pinPath = data.folderRelativePathNoVfsSuffix();
+        data.folder->vfs().setPinState(pinPath, PinState::AlwaysLocal);
 
         // Trigger the recursive download
         data.folder->downloadVirtualFile(data.folderRelativePath);
@@ -720,9 +719,8 @@ void SocketApi::command_MAKE_ONLINE_ONLY(const QString &filesArg, SocketListener
             continue;
 
         // Update the pin state on all items
-        auto pinPath = data.folderRelativePathNoVfsSuffix().toUtf8();
-        data.folder->journalDb()->wipePinStateForPathAndBelow(pinPath);
-        data.folder->journalDb()->setPinStateForPath(pinPath, PinState::OnlineOnly);
+        auto pinPath = data.folderRelativePathNoVfsSuffix();
+        data.folder->vfs().setPinState(pinPath, PinState::OnlineOnly);
 
         // Trigger recursive dehydration
         data.folder->dehydrateFile(data.folderRelativePath);
@@ -1019,7 +1017,7 @@ void SocketApi::command_GET_MENU_ITEMS(const QString &argument, OCC::SocketListe
         for (const auto &file : files) {
             auto fileData = FileData::get(file);
             auto path = fileData.folderRelativePathNoVfsSuffix();
-            auto pinState = syncFolder->journalDb()->effectivePinStateForPath(path.toUtf8());
+            auto pinState = syncFolder->vfs().getPinState(path);
             if (!pinState) {
                 // db error
                 hasAlwaysLocal = true;