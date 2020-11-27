gergap/client

PropagateUpload:  Avoid many allocations by using QByteArrayLiteral

src/libsync/propagateuploadng.cpp
@@ -503,17 +503,17 @@ void PropagateUploadFileNG::doFinalMove()
     auto headers = PropagateUploadFileCommon::headers();
 
     // "If-Match applies to the source, but we are interested in comparing the etag of the destination
-    auto ifMatch = headers.take("If-Match");
+    auto ifMatch = headers.take(QByteArrayLiteral("If-Match"));
     if (!ifMatch.isEmpty()) {
-        headers["If"] = "<" + destination.toUtf8() + "> ([" + ifMatch + "])";
+        headers[QByteArrayLiteral("If")] = "<" + destination.toUtf8() + "> ([" + ifMatch + "])";
     }
     if (!_transmissionChecksumHeader.isEmpty()) {
         headers[checkSumHeaderC] = _transmissionChecksumHeader;
     }
-    headers["OC-Total-Length"] = QByteArray::number(_bytesToUpload);
-    headers["OC-Total-File-Length"] = QByteArray::number(_item->_size);
+    headers[QByteArrayLiteral("OC-Total-Length")] = QByteArray::number(_bytesToUpload);
+    headers[QByteArrayLiteral("OC-Total-File-Length")] = QByteArray::number(_item->_size);
 
-    QUrl source = _zsyncSupported ? Utility::concatUrlPath(chunkUrl(), "/.file.zsync") : Utility::concatUrlPath(chunkUrl(), "/.file");
+    QUrl source = _zsyncSupported ? Utility::concatUrlPath(chunkUrl(), QStringLiteral("/.file.zsync")) : Utility::concatUrlPath(chunkUrl(), QStringLiteral("/.file"));
 
     auto job = new MoveJob(propagator()->account(), source, destination, headers, this);
     _jobs.append(job);

briankendall/desktop

src/libsync/propagateuploadng.cpp
@@ -295,15 +295,15 @@ void PropagateUploadFileNG::startNextChunk()
         auto headers = PropagateUploadFileCommon::headers();
 
         // "If-Match applies to the source, but we are interested in comparing the etag of the destination
-        auto ifMatch = headers.take("If-Match");
+        auto ifMatch = headers.take(QByteArrayLiteral("If-Match"));
         if (!ifMatch.isEmpty()) {
-            headers["If"] = "<" + QUrl::toPercentEncoding(destination, "/") + "> ([" + ifMatch + "])";
+            headers[QByteArrayLiteral("If")] = "<" + QUrl::toPercentEncoding(destination, "/") + "> ([" + ifMatch + "])";
         }
         if (!_transmissionChecksumHeader.isEmpty()) {
             qCInfo(lcPropagateUpload) << destination << _transmissionChecksumHeader;
             headers[checkSumHeaderC] = _transmissionChecksumHeader;
         }
-        headers["OC-Total-Length"] = QByteArray::number(fileSize);
+        headers[QByteArrayLiteral("OC-Total-Length")] = QByteArray::number(fileSize);
 
         auto job = new MoveJob(propagator()->account(), Utility::concatUrlPath(chunkUrl(), "/.file"),
             destination, headers, this);