ckamm/mirall

PinStates cleanup
- SyncJournalDB functions now behind internalPinStates() to avoid
accidental usage, when nearly everyone should go through Vfs.
- Rename Vfs::getPinState() to Vfs::pinState()

src/gui/socketapi.cpp
@@ -911,7 +911,7 @@ void SocketApi::command_GET_MENU_ITEMS(const QString &argument, OCC::SocketListe
         for (const auto &file : files) {
             auto fileData = FileData::get(file);
             auto path = fileData.folderRelativePathNoVfsSuffix();
-            auto pinState = folder->vfs().getPinState(path);
+            auto pinState = folder->vfs().pinState(path);
             if (!pinState) {
                 // db error
                 hasAlwaysLocal = true;

briankendall/desktop

src/gui/socketapi.cpp
@@ -1017,7 +1017,7 @@ void SocketApi::command_GET_MENU_ITEMS(const QString &argument, OCC::SocketListe
         for (const auto &file : files) {
             auto fileData = FileData::get(file);
             auto path = fileData.folderRelativePathNoVfsSuffix();
-            auto pinState = syncFolder->vfs().getPinState(path);
+            auto pinState = syncFolder->vfs().pinState(path);
             if (!pinState) {
                 // db error
                 hasAlwaysLocal = true;